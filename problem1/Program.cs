8f055cc147e36d8be5f348d955048f6eb747ffd5
Anastasia <zaytseva7821@ya.ru>
2022-09-06 18:24:16 +0400

    problem1

---
+++ problem1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(System.Console.ReadLine());
+if (num < 0)
+num = num * -1;
+if (num < 100 || num > 999)
+{
+    System.Console.WriteLine("Введено некорректное число");
+}
+else
+{
+    System.Console.WriteLine(num/10-num/100*10);
+}
